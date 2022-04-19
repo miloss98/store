0ecb7e7d75354dd6113d596a9054478bb3cb047d
stojanovic98m <stojanovic98m@gmail.com>
2022-04-20 00:24:42 +0200

    axios installed

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,28 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
 const App = () => {
+  const [data, setData] = useState([]);
+  const url = "https://fakestoreapi.com/productsssdas";
+
+  const fetchData = async () => {
+    try {
+      const request = await axios.get(url, {
+        headers: {
+          accept: "application/json",
+        },
+      });
+      const response = request.data;
+      setData(response);
+    } catch (error) {
+      console.log(error.response.status);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
     <div>
       <h1>Store app</h1>

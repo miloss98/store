51c9ff26a11ac6c8c764112e8b7c63ec058708de
stojanovic98m <stojanovic98m@gmail.com>
2022-05-09 14:24:55 +0200

    about page done

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,5 +1,24 @@
+import StyledAboutPage, {
+  StyledAboutContainer,
+} from "../styled/StyledAboutPage";
+
 const AboutPage = () => {
-  return <div>AboutPage</div>;
+  return (
+    <StyledAboutPage>
+      <StyledAboutContainer>
+        <div>
+          <h1>About page</h1>
+          <p> Informations: </p>
+          <p>
+            {" "}
+            This is a React.js project. Project is made of external libraries
+            and other tools like: React Router DOM, Styled Components, Axios and
+            Fake Store API{" "}
+          </p>
+        </div>
+      </StyledAboutContainer>
+    </StyledAboutPage>
+  );
 };
 
 export default AboutPage;
--- src/styled/StyledAboutPage.jsx
+++ src/styled/StyledAboutPage.jsx
@@ -1,7 +1,44 @@
 import styled from "styled-components";
 
 const StyledAboutPage = styled.div`
+  width: 100vw;
+  height: 86vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
   background-color: var(--background);
+`;
+
+export const StyledAboutContainer = styled.section`
+  width: 60vw;
+  height: 60vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: center;
+  background-color: var(--item-background);
+
+  div {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    width: 70%;
+    height: 70%;
+    text-align: center;
+
+    h1 {
+      padding: 3rem 0rem 5rem 0rem;
+      font-size: 2rem;
+      font-weight: 600;
+    }
+    p {
+      font-size: 1.2rem;
+      letter-spacing: 2px;
+      padding-bottom: 1.5rem;
+    }
+  }
 `;
 
 export default StyledAboutPage;

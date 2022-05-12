23d8eea36c23d6045b121cf060e9bb9a35c747de
stojanovic98m <stojanovic98m@gmail.com>
2022-05-12 18:13:08 +0200

    fetching single product info

--- src/pages/SingleProductPage.jsx
+++ src/pages/SingleProductPage.jsx
@@ -1,5 +1,36 @@
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import axios from "axios";
+
+const url = "https://fakestoreapi.com/products";
+
 const SingleProductPage = () => {
-  return <div>SingleProductPage</div>;
+  const [product, setProduct] = useState([]);
+  const { productID } = useParams();
+
+  const getSingleProduct = async () => {
+    const response = await axios.get(`${url}/${productID}`);
+    const singleProduct = response.data;
+    const { id, title, price, category, description, image, rating } =
+      singleProduct;
+    setProduct(singleProduct);
+  };
+
+  useEffect(() => {
+    getSingleProduct();
+  }, []);
+
+  return (
+    <section key={product.id}>
+      <h1> {product.title}</h1>
+      <img src={product.image} alt={product.title} />
+      <p> price: {product.price} </p>
+      <p> rating: {product.rating?.rate} </p>
+      <p> count: {product.rating?.count} </p>
+      <p> {product.description} </p>
+      <p> {product.category} </p>
+    </section>
+  );
 };
 
 export default SingleProductPage;
